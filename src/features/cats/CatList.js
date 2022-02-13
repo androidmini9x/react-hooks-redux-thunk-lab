df26621b6555591ab0034aae1315c88ac8b2cf83
androidmini9x <androidmini92@gmail.com>
2022-02-13 03:18:39 +0300

    Solved

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,12 @@
 // write your CatList component here
+function CatList({catPics}) {
+	return (
+		<div>
+			{catPics.map(catPic => {
+				return <img key={catPic.id} src={catPic.url} alt="cat" />
+			})}
+		</div>
+	);
+}
+
+export default CatList;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "react-thunk";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import catsReducer from "./features/cat/catsSlice";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(catsReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>, document.getElementById("root"));
